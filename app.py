b59c5f74605617bdc2d7c70740566c8eefcffe3b
ramigalv60 <104180859+ramigalv60@users.noreply.github.com>
2024-08-19 00:11:40 -0300

    wait until retrain to test

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-import pandas as pd
 import numpy as np
 import tensorflow as tf
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
 from PIL import Image
 from tensorflow.keras.models import load_model
+from io import BytesIO
 
 class CFG:
     image_size = (224, 224)  # Example image size, adjust as needed
@@ -15,17 +15,15 @@ tf.random.set_seed(CFG.seed)
 
 app = FastAPI()
 
-# Load the model
+# Load the model without custom objects
 model = load_model('models/model.h5')
+#tf.keras.layers.TFSMLayer("FOLDER_NAME", call_endpoint="serving_default")
 
-class PredictionRequest(BaseModel):
-    image_path: str
-
-def load_and_preprocess_image(image_path):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, CFG.image_size)
-    image = image / 255.0  # Normalize image
+def load_and_preprocess_image(image_data):
+    image = Image.open(BytesIO(image_data))
+    image = image.resize(CFG.image_size)
+    image = np.array(image) / 255.0  # Manually normalize image
+    image = np.expand_dims(image, axis=0)  # Add batch dimension
     return image
 
 @app.get("/")
@@ -33,9 +31,9 @@ def read_root():
     return {"message": "Welcome to the model prediction API!"}
 
 @app.post("/predict")
-def predict(request: PredictionRequest):
-    image = load_and_preprocess_image(request.image_path)
-    image = np.expand_dims(image, axis=0)
+async def predict(file: UploadFile = File(...)):
+    image_data = await file.read()
+    image = load_and_preprocess_image(image_data)
     prediction = model.predict(image)
     binary_prediction = int(prediction[0][0] > 0.5)
     return {"prediction": binary_prediction}
