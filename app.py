bdbde17612b9d60970ef4275de0cbb365fe20a2f
ramigalv60 <104180859+ramigalv60@users.noreply.github.com>
2024-08-25 03:23:03 -0300

    IT WORKS

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.image import img_to_array
 from io import BytesIO
-
 class CFG:
     image_size_melanoma = (224, 224) 
-    image_size_lesion = (255, 255)
+    image_size_lesion = (256, 256)
     seed = 42
 
 tf.random.set_seed(CFG.seed)
@@ -35,11 +34,21 @@ def load_and_preprocess_image_melanoma(image_data):
     image_array = np.expand_dims(image_array, axis=0)
     return image_array
 
+def load_and_preprocess_image_lesiones(image_data):
+    image = Image.open(BytesIO(image_data))
+
+    image = image.convert('RGB')
+    image = image.resize(CFG.image_size_lesion)
+    image_array = img_to_array(image)
+    image_array = image_array / 255.0
+    image_array = np.expand_dims(image_array, axis=0)
+    return image_array
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the model prediction API!"}
 
-@app.post("/predict/")
+@app.post("/predict/melanoma")
 async def predict(file: UploadFile = File(...)):
     try:
         model = load_model('models/my_model.keras')
@@ -49,6 +58,18 @@ async def predict(file: UploadFile = File(...)):
         prediction = prediction.tolist()
         return {"prediction": prediction}
         #return {"prediction": classes[np.argmax(prediction)]}
+    except Exception as e:
+        return {"error": str(e)}
+    
+@app.post("/predict/lesiones")
+async def predict(file: UploadFile = File(...)):
+    try:
+        model = load_model('models/bestmodel.keras')
+        image_data = await file.read()
+        image = load_and_preprocess_image_lesiones(image_data)
+        prediction = model.predict(image)
+        prediction = prediction.tolist()
+        return {"prediction": classes[np.argmax(prediction)]}
     except Exception as e:
         return {"error": str(e)}
 
